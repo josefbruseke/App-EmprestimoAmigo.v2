9a83433e445b88b0f4d14004306d382d235f26d7
Josef Bruseke <josefbruseke@gmail.com>
2024-02-17 15:42:09 -0300

    Spring Boot initialization

---
+++ EmprestimoAmigo/src/main/java/com/EmprestimoAmigo/EmprestimoAmigo/EmprestimoAmigoApplication.java
@@ -0,0 +1,13 @@
+package com.EmprestimoAmigo.EmprestimoAmigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoAmigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoAmigoApplication.class, args);
+	}
+
+}
---
+++ EmprestimoAmigo/src/test/java/com/EmprestimoAmigo/EmprestimoAmigo/EmprestimoAmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmprestimoAmigo.EmprestimoAmigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoAmigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

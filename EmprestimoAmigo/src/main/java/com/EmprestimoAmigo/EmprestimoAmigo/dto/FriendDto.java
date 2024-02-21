cab0f6a92950a09baadfdc132fd3062b7fdf9648
Josef Bruseke <josefbruseke@gmail.com>
2024-02-21 14:25:00 -0300

    Creation FriendDto

---
+++ EmprestimoAmigo/src/main/java/com/EmprestimoAmigo/EmprestimoAmigo/dto/FriendDto.java
@@ -0,0 +1,16 @@
+package com.EmprestimoAmigo.EmprestimoAmigo.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class FriendDto {
+    private Long id;
+    private String name;
+    private String phone;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+}
